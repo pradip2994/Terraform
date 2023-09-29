2833a19c4a4e8d7853ca5cc0fa36cc2708bf9c20
pradip2994 <124191442+pradip2994@users.noreply.github.com>
2023-09-30 03:12:55 +0530

    Create user_data1.sh

---
+++ Terraform-state_Backend/user_data1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+####
+# NAME: PRADIP KUMAR
+# THIS IS A SCRIPT TO GET RESPONSE FROM SERVER TO CKECK LOADEBALANCER IS WORKING.  
+####
+
+#Install apache2
+
+apt-get update -y
+apt-get install -y apache2
+
+echo "<h1>Hello, This is a response from the server IP address - $(hostname -f)</h1>" > /var/www/html/index.html
